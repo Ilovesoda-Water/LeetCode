434410816d4dbfd038613782d2a5b79c6eda580f
shuangqinbuaa <qinshuang@buaa.edu.cn>
2019-01-24 17:52:04 +0800

    shuangqin

--- tmp.cpp
+++ tmp.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
-int main(){
-    printf("hello, world\n");
-    return 0;
-}
+ListNode *p1, *p2;
+        while(curr!=NULL){
+            count = 0;
+            p1 = curr; p2 = curr;
+            while(p2!=NULL&&count<k-1){
+                p2 = p2->next;
+                count++;
+            }
+            if (p2!=NULL){
+                curr = p2->next;
+                reverseList(p1,p2);
+                begin->next = p2;
+                begin = p1;
+            }
+        }
